e652f6ce53f854639652390b1d6dcb6dd17b68f9
tonkec <cdkeys.thisemail@gmail.com>
2024-09-18 00:46:14 +0200

    Dummy dataset file

---
+++ dataset.py
@@ -0,0 +1,30 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+
+class SentimentDataset(Dataset):
+    def __init__(self, data, tokenizer, max_len=128):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        text, label = self.data[idx]
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            padding="max_length",
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors="pt",
+        )
+        return {
+            "input_ids": encoding["input_ids"].flatten(),
+            "attention_mask": encoding["attention_mask"].flatten(),
+            "label": torch.tensor(label, dtype=torch.long),
+        }
